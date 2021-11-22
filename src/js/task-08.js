2c82c9c466550e59fd6a0b6360ff60b65795cfcb
Ihor Khomenko <igorhomenko97@gmail.com>
2021-11-22 21:16:15 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены.");
+  }
+
+  console.log("Email input: ", email.value);
+  console.log("Password input: ", password.value);
+  event.currentTarget.reset();
+}
